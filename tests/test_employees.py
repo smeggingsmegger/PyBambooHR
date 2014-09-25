34014119c7b259e720f6fa3c2809694bf43fc3ae
Ng Zhi An <ngzhian@gmail.com>
2014-09-25 18:40:53 -0400

    Formatting fixes to test

--- tests/test_employees.py
+++ tests/test_employees.py
@@ -19,7 +19,7 @@ from requests import HTTPError
 # Force parent directory onto path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from PyBambooHR import PyBambooHR, utils
+from PyBambooHR import PyBambooHR
 
 class test_employees(unittest.TestCase):
     # Used to store the cached instance of PyBambooHR
@@ -37,89 +37,89 @@ class test_employees(unittest.TestCase):
 
     @httpretty.activate
     def test_get_employee_directory(self):
-        body = {"fields":[
-        {
-            "id":"displayName",
-            "type":"text",
-            "name":"Display name"
-        },
-        {
-            "id":"firstName",
-            "type":"text",
-            "name":"First name"
-        },
-        {
-            "id":"lastName",
-            "type":"text",
-            "name":"Last name"
-        },
-        {
-            "id":"jobTitle",
-            "type":"list",
-            "name":"Job title"
-        },
-        {
-            "id":"workPhone",
-            "type":"text",
-            "name":"Work Phone"
-        },
-        {
-            "id":"workPhoneExtension",
-            "type":"text",
-            "name":"Work Extension"
-        },
-        {
-            "id":"mobilePhone",
-            "type":"text",
-            "name":"Mobile Phone"
-        },
-        {
-            "id":"workEmail",
-            "type":"email",
-            "name":"Work Email"
-        },
-        {
-            "id":"department",
-            "type":"list",
-            "name":"Department"
-        },
-        {
-            "id":"location",
-            "type":"list",
-            "name":"Location"
-        },
-        {
-            "id":"division",
-            "type":"list",
-            "name":"Division"
-        },
-        {
-            "id":"photoUploaded",
-            "type":"bool",
-            "name":"Employee photo exists"
-        },
-        {
-            "id":"photoUrl",
-            "type":"url",
-            "name":"Employee photo url"
-        }
-        ],"employees":[
-        {
-        "id":"123",
-        "displayName":"Test Person",
-        "firstName":"Test",
-        "lastName":"Person",
-        "jobTitle":"Testing Coordinator",
-        "workPhone":"555-555-5555",
-        "workPhoneExtension":"",
-        "mobilePhone":"555-555-5555",
-        "workEmail":"test@testperson.com",
-        "department":"Useless Department",
-        "location":"Testville, US",
-        "division": None,
-        "photoUploaded": False,
-        "photoUrl":"https://iws.bamboohr.com/images/photo_placeholder.gif"
-        }]
+        body = {"fields": [
+            {
+                "id": "displayName",
+                "type": "text",
+                "name": "Display name"
+            },
+            {
+                "id": "firstName",
+                "type": "text",
+                "name": "First name"
+            },
+            {
+                "id": "lastName",
+                "type": "text",
+                "name": "Last name"
+            },
+            {
+                "id": "jobTitle",
+                "type": "list",
+                "name": "Job title"
+            },
+            {
+                "id": "workPhone",
+                "type": "text",
+                "name": "Work Phone"
+            },
+            {
+                "id": "workPhoneExtension",
+                "type": "text",
+                "name": "Work Extension"
+            },
+            {
+                "id": "mobilePhone",
+                "type": "text",
+                "name": "Mobile Phone"
+            },
+            {
+                "id": "workEmail",
+                "type": "email",
+                "name": "Work Email"
+            },
+            {
+                "id": "department",
+                "type": "list",
+                "name": "Department"
+            },
+            {
+                "id": "location",
+                "type": "list",
+                "name": "Location"
+            },
+            {
+                "id": "division",
+                "type": "list",
+                "name": "Division"
+            },
+            {
+                "id": "photoUploaded",
+                "type": "bool",
+                "name": "Employee photo exists"
+            },
+            {
+                "id": "photoUrl",
+                "type": "url",
+                "name": "Employee photo url"
+            }],
+            "employees": [
+                {
+                    "id": "123",
+                    "displayName": "Test Person",
+                    "firstName": "Test",
+                    "lastName": "Person",
+                    "jobTitle": "Testing Coordinator",
+                    "workPhone": "555-555-5555",
+                    "workPhoneExtension": "",
+                    "mobilePhone": "555-555-5555",
+                    "workEmail": "test@testperson.com",
+                    "department": "Useless Department",
+                    "location": "Testville, US",
+                    "division":  None,
+                    "photoUploaded":  False,
+                    "photoUrl": "https://iws.bamboohr.com/images/photo_placeholder.gif"
+                }]
         }
         body = dumps(body)
         httpretty.register_uri(httpretty.GET, "https://api.bamboohr.com/api/gateway.php/test/v1/employees/directory",
@@ -230,14 +230,15 @@ class test_employees(unittest.TestCase):
                          <field id="customTypeC">Value C</field>
                      </row>
                  </table>"""
-        httpretty.register_uri(httpretty.GET, "https://api.bamboohr.com/api/gateway.php/test/v1/employees/123/tables/customTable",
+        httpretty.register_uri(httpretty.GET,
+                               "https://api.bamboohr.com/api/gateway.php/test/v1/employees/123/tables/customTable",
                                body=xml,
                                content_type="application/xml")
-
         table = self.bamboo.get_tabular_data('customTable', 123)
         d = {'123': [{'customTypeA': 'Value A',
                       'customTypeB': 'Value B',
                       'customTypeC': 'Value C'}]}
+
         self.assertIsNotNone(table)
         self.assertEqual(d, table)
 
@@ -256,10 +257,10 @@ class test_employees(unittest.TestCase):
                          <field id="customTypeC">333 Value C</field>
                      </row>
                  </table>"""
-        httpretty.register_uri(httpretty.GET, "https://api.bamboohr.com/api/gateway.php/test/v1/employees/all/tables/customTable",
+        httpretty.register_uri(httpretty.GET,
+                               "https://api.bamboohr.com/api/gateway.php/test/v1/employees/all/tables/customTable",
                                body=xml,
                                content_type="application/xml")
-
         table = self.bamboo.get_tabular_data('customTable')
         d = {'123': [{'customTypeA': 'Value A',
                       'customTypeB': 'Value B',
@@ -268,41 +269,40 @@ class test_employees(unittest.TestCase):
                       'customTypeB': '333 Value B',
                       'customTypeC': '333 Value C'}]}
 
-        self.maxDiff = 10000
         self.assertIsNotNone(table)
         self.assertEqual(d, table)
-        # self.assertIn('<row id="321" employeeId="123">', table)
-        # self.assertIn('<field id="customTypeA">123 Value A</field>', table)
-        # self.assertIn('<field id="customTypeB">123 Value B</field>', table)
-        # self.assertIn('<field id="customTypeC">123 Value C</field>', table)
-        # self.assertIn('<row id="322" employeeId="333">', table)
-        # self.assertIn('<field id="customTypeA">333 Value A</field>', table)
-        # self.assertIn('<field id="customTypeB">333 Value B</field>', table)
-        # self.assertIn('<field id="customTypeC">333 Value C</field>', table)
 
     @httpretty.activate
     def test_add_row(self):
-        httpretty.register_uri(httpretty.POST, "https://api.bamboohr.com/api/gateway.php/test/v1/employees/333/tables/customTable/",
-                               body='', status='200')
+        httpretty.register_uri(httpretty.POST,
+                               "https://api.bamboohr.com/api/gateway.php/test/v1/employees/333/tables/customTable/",
+                               body='',
+                               status='200')
 
         row = {'custom_type_a': 'New Value A'}
         result = self.bamboo.add_row('customTable', '333', row)
+
         self.assertTrue(result)
 
     @httpretty.activate
     def test_add_row_failure(self):
         # Forbidden result
-        httpretty.register_uri(httpretty.POST, "https://api.bamboohr.com/api/gateway.php/test/v1/employees/123/tables/customTable/",
-                               body='', status='406')
+        httpretty.register_uri(httpretty.POST,
+                               "https://api.bamboohr.com/api/gateway.php/test/v1/employees/123/tables/customTable/",
+                               body='',
+                               status='406')
         row = {'invalid_id': 'New Value A'}
+
         self.assertRaises(HTTPError, self.bamboo.add_row, 'customTable', '123', row)
 
     @httpretty.activate
     def test_update_row(self):
-        httpretty.register_uri(httpretty.POST, "https://api.bamboohr.com/api/gateway.php/test/v1/employees/333/tables/customTable/321/",
-                               body='', status='200')
-
+        httpretty.register_uri(httpretty.POST,
+                               "https://api.bamboohr.com/api/gateway.php/test/v1/employees/333/tables/customTable/321/",
+                               body='',
+                               status='200')
         row = {'custom_type_a': 'New Value A'}
         result = self.bamboo.update_row('customTable', '333', '321', row)
+
         self.assertTrue(result)
 
