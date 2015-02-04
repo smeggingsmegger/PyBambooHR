2fb6c045a8dd40533d2044fa88e0e0901fc17c55
Scott Blevins <sblevins@gmail.com>
2015-02-04 15:27:39 -0600

    Bumping version.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if os.path.exists('README.txt'):
 
 setup(
     name='PyBambooHR',
-    version='0.4.2',
+    version='0.5.0',
     url='http://github.com/smeggingsmegger/PyBambooHR',
     license='MIT',
     author='Scott Blevins',
